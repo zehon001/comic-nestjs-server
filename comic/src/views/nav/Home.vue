5a94303ca52fef3cf96f0de9556368911f50e4d6
黄梓恒 <azusako@huangzihengdeMacBook-Pro.local>
2020-02-18 22:11:36 +0800

    主题默认暗色，reduce空数组bug，去除首页UI

--- comic/src/views/nav/Home.vue
+++ comic/src/views/nav/Home.vue
@@ -1,38 +1,6 @@
 <template>
 	<v-container class="fill-height">
-		<div style="position: absolute;top: 0;left: 0;">首页</div>
-		<v-row
-			align="center"
-			justify="center"
-		>
-			<v-tooltip right>
-				<template v-slot:activator="{ on }">
-					<v-btn
-						icon
-						large
-						target="_blank"
-						v-on="on"
-					>
-						<v-icon large>mdi-code-tags</v-icon>
-					</v-btn>
-				</template>
-				<span>Source</span>
-			</v-tooltip>
-			<v-tooltip right>
-				<template v-slot:activator="{ on }">
-					<v-btn
-						icon
-						large
-						href="https://codepen.io/johnjleider/pen/MNYLdL"
-						target="_blank"
-						v-on="on"
-					>
-						<v-icon large>mdi-codepen</v-icon>
-					</v-btn>
-				</template>
-				<span>Codepen</span>
-			</v-tooltip>
-		</v-row>
+		<div>首页</div>
 	</v-container>
 </template>
 
