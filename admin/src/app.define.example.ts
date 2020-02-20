4f1837492d9746739925f95c4a9409daf0b213aa
黄梓恒 <azusako@huangzihengdeMacBook-Pro.local>
2020-02-20 15:54:27 +0800

    修改示例配置的地址

--- admin/src/app.define.example.ts
+++ admin/src/app.define.example.ts
@@ -1,4 +1,4 @@
 export default class AppDF {
 	/**服务器地址 */
-	static readonly SERVER_URL = "http://localhost:9030/comic_api";
+	static readonly SERVER_URL = "http://localhost:9030/comic_admin_api";
 }
